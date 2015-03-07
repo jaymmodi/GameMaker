ff555087c4f2521320d8a0c4e6c2883a44040f65
Nayana <ncharwad@umail.iu.edu>
2015-03-07 10:20:57 -0500

    This change is implemented so as to add game loss condition in game maker application. User can now select game loss condition for multiple sprites.
    JIRA issue : http://tintin.cs.indiana.edu:8091/browse/PSTAB-18

--- src/action/Actions.java
+++ src/action/Actions.java
@@ -13,7 +13,7 @@ public enum Actions {
 	MOVE(new MoveSprite()), LEFTMOVE(new MoveSpriteLeft()), RIGHTMOVE(new MoveSpriteRight()), DISAPPEAR(new Disappear()), CHANGEDIRECTION(
 			new ChangeDirection()), SOUND(new PlaySound()), UPMOVE(new MoveSpriteUp()), BOTTOMMOVE(new MoveSpriteDown()), AUTOMOVEUP(
 			new AutoMoveUp()), AUTOMOVEDOWN(new AutoMoveDown()), AUTOMOVERIGHT(new AutoMoveRight()), AUTOMOVELEFT(
-			new AutoMoveLeft()), FIREACTION(new FireAction()), GAMEWIN(new GameWin());
+			new AutoMoveLeft()), FIREACTION(new FireAction()), GAMEWIN(new GameWin()), GAMELOSS(new GameLoss());
 
 	private Action instance;
 
--- src/controller/GameMakerController.java
+++ src/controller/GameMakerController.java
@@ -270,7 +270,7 @@ public class GameMakerController {
 
 				String[] collisionEventSubType = spriteNames.toArray(new String[spriteNames.size()]);
 
-				String[] collisionAction = new String[] { "Disappear", "ChangeDirection", "Sound", "GameWin" };
+				String[] collisionAction = new String[] { "Disappear", "ChangeDirection", "Sound", "GameWin", "GameLoss" };
 				String[] keyboardAction = new String[] { "LeftMove", "RightMove", "UpMove", "DownMove", "Fire"};
 				String[] timeChangeAction = new String[] { "Move", "AutoMoveLeft", "AutoMoveRight",
 						 "AutoMoveUp" , "AutoMoveDown"};
